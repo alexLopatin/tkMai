37e7f3ff32c674a8c131cd4871b37058cf5847f7
alexLopatin <minelabory@gmail.com>
2017-10-23 22:29:35 +0300

    fixed white space on mobile devices

--- files/sideMenu.js
+++ files/sideMenu.js
@@ -1,6 +1,41 @@
 var objectsOpened = {}
 
 
+function ResizeContent()
+{
+	var footer = document.getElementsByClassName('site-footer')[0].clientHeight;
+	 footer = document.getElementsByClassName('site-footer')[0].offsetHeight;
+	 footer = document.getElementsByClassName('site-footer')[0].scrollHeight;
+
+
+	var header = document.getElementsByClassName('site-header')[0].clientHeight;
+	 header = document.getElementsByClassName('site-header')[0].offsetHeight;
+	 header = document.getElementsByClassName('site-header')[0].scrollHeight;
+
+
+	var h = window.innerHeight;
+
+	var content = document.getElementsByClassName('container')[0].clientHeight;
+	 content = document.getElementsByClassName('container')[0].offsetHeight;
+	 content = document.getElementsByClassName('container')[0].scrollHeight;
+
+	if(h>=content+footer+header)
+		document.getElementsByClassName("container")[0].style.height = (h -footer-header)+"px";	
+	else
+	{
+		console.log(h);
+		console.log(content+footer+header);
+		document.getElementsByClassName("container")[0].style.height = "100%";	
+	}
+}
+
+ResizeContent();
+window.onresize = function(event) {
+	console.log("resized");
+    ResizeContent();
+};
+
+
 function openNav() {
     document.getElementsByClassName("SideNav")[0].style.width = "250px";
 }
